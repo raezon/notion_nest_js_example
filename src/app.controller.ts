19c4c92fa2c4f08ffb79b56691b4729c04e325cd
raezon <amardjebabla10@gmail.com>
2023-07-16 17:24:53 +0100

    Applying logic to assign employee to the desired mission

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,6 +8,12 @@ export class AppController {
   @Render('index')
   async root() {
     const interventions = await this.appService.getIntervention();
-    return { interventions: interventions };
+    const interventionsJSON = JSON.stringify(interventions);
+    console.log(interventionsJSON);
+    return { interventions: interventions, interventionsJSON };
+  }
+  @Post('planifier')
+  async planifier(@Body() data: any) {
+    return this.appService.planifier(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -10,22 +10,40 @@ export class AppService {
     try {
       const notion = new Client({ auth: process.env.NOTION_TOKEN });
       const response = await notion.databases.query({
-        database_id: process.env.DATABASE_ID,
+        database_id: process.env.DATABASE_ID_INTERVENTION,
       });
 
-      const interventionList = response.results.map((page) => {
+      const interventionList = await response.results.map(async (page) => {
         const { id, properties } = page as any; // Type assertion
 
         const nom = properties?.nom?.rich_text?.[0]?.plain_text;
-        console.log(nom);
+        const siteIds =
+          properties?.site?.relation?.map((relation) => relation.id) || [];
+        const sitePromises = siteIds.map((siteId) =>
+          notion.pages.retrieve({ page_id: siteId }),
+        );
+        const sitePages = await Promise.all(sitePromises);
+
+        const site = sitePages.map((sitePage) => {
+          return {
+            id: sitePage.id,
+            address:
+              sitePage.properties?.Addresse?.rich_text?.[0]?.plain_text ||
+              'N/A',
+            latitude: sitePage.properties?.Latitude?.number || null,
+            longitude: sitePage.properties?.Longitude?.number || null,
+          };
+        });
+
         return {
           id,
           nom,
-          // Include other properties in the returned object
+          latitude: site[0].latitude,
+          longitude: site[0].longitude,
         };
       });
-
-      return interventionList;
+      const interventionListPromises = await Promise.all(interventionList);
+      return interventionListPromises;
     } catch (error) {
       if (error.code === APIErrorCode.ObjectNotFound) {
         // Handle the error when the database is not found
@@ -34,5 +52,136 @@ export class AppService {
         console.error(error);
       }
     }
+  }
+  async getInterventionList() {
+    const notion = new Client({ auth: process.env.NOTION_TOKEN });
+    const response = await notion.databases.query({
+      database_id: process.env.DATABASE_ID_INTERVENTION,
+    });
+    const interventionList = await response.results.map(async (page) => {
+      const { id, properties } = page as any; // Type assertion
+
+      const nom = properties?.nom?.rich_text?.[0]?.plain_text;
+      const date_heure = properties?.date_heure?.date?.start || null;
+      // Retrieve the related "site" pages
+      const siteIds =
+        properties?.site?.relation?.map((relation) => relation.id) || [];
+      const sitePromises = siteIds.map((siteId) =>
+        notion.pages.retrieve({ page_id: siteId }),
+      );
+      const sitePages = await Promise.all(sitePromises);
+
+      const site = sitePages.map((sitePage) => {
+        return {
+          id: sitePage.id,
+          address:
+            sitePage.properties?.Addresse?.rich_text?.[0]?.plain_text || 'N/A',
+          latitude: sitePage.properties?.Latitude?.number || null,
+          longitude: sitePage.properties?.Longitude?.number || null,
+        };
+      });
+
+      return {
+        id,
+        nom,
+        site,
+        // Include other properties in the returned object
+      };
+    });
+    return await Promise.all(interventionList);
+  }
+  async getTechnicienList() {
+    const notion = new Client({ auth: process.env.NOTION_TOKEN });
+    const response = await notion.databases.query({
+      database_id: process.env.DATABASE_ID_TECHNICIEN,
+    });
+    const technicienList = await response.results.map(async (page) => {
+      const { id, properties } = page as any; // Type assertion
+
+      const nom = properties?.Nom?.rich_text?.[0]?.plain_text;
+      const prénom = properties?.Prénom?.rich_text?.[0]?.plain_text;
+      const latitude = properties?.latitude?.number;
+      const longitude = properties?.longitude?.number;
+      // Retrieve the related "site" pages
+
+      return {
+        id,
+        nom,
+        prénom,
+        latitude,
+        longitude,
+        // Include other properties in the returned object
+      };
+    });
+
+    return await Promise.all(technicienList);
+  }
+
+  async planifier(intervention) {
+    try {
+      const startTime = new Date().getTime(); // Start timestamp
+
+      const technicienList = await this.getTechnicienList();
+
+      const {
+        latitude: interventionLatitude,
+        longitude: interventionLongitude,
+      } = intervention;
+      let nearestTechnician = null;
+      let nearestDistance = Infinity;
+
+      for (const technician of technicienList) {
+        const { latitude: technicianLatitude, longitude: technicianLongitude } =
+          technician;
+        const distance = this.calculateDistance(
+          interventionLatitude,
+          interventionLongitude,
+          technicianLatitude,
+          technicianLongitude,
+        );
+
+        if (distance < nearestDistance) {
+          nearestTechnician = technician;
+          nearestDistance = distance;
+        }
+      }
+
+      const assignedTechnicians = {
+        interventionNom: intervention.interventionNom,
+        technicianId: nearestTechnician.id,
+        technicianNom: nearestTechnician.nom,
+        technicianPrénom: nearestTechnician.prénom,
+        // Include other properties in the returned object
+      };
+
+      const endTime = new Date().getTime(); // End timestamp
+      const executionTime = endTime - startTime; // Execution time in milliseconds
+
+      return { assignedTechnicians, executionTime };
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+    }
+  }
+
+  // Helper function to calculate the distance between two coordinates using the Haversine formula
+  calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) *
+        Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+
+    return distance;
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI / 180);
   }
 }
